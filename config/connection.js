c6e02177061b76656b322f94e99eee4c2a942afb
Ever's PC <gomezsantiagoeverardo@gmail.com>
2020-02-01 14:47:39 -0800

    attempting to fix heroku and mysql connectivity issue

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
 var mysql = require("mysql");
 var connection;
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "password",
-        database: "burgers_db"
-    });
-}
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// }
+// else {
+connection = mysql.createConnection({
+    host: "k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    user: "xe5f4lyk9xkhtyym",
+    password: "sb9oibkrrto4cd5q",
+    database: "fpqelp6od6vee3tw"
+});
+// }
 connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
@@ -20,4 +20,4 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId);
 });
 
-module.exports = connection;  
+module.exports = connection;
